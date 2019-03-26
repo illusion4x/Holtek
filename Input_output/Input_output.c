b22de21adc63beb34fcfe0b50523223433e11aa3
my_mac2017 <mexuc@qq.com>
2019-03-26 14:10:28 +0800

    input_output finish

--- Input_output/Input_output.c
+++ Input_output/Input_output.c
@@ -4,6 +4,7 @@
 #define KEY1 _pc4                                                    
 
 typedef unsigned int u16;
+typedef unsigned int u8;
 
 void delay_ms(u16 ms){
 	while(ms--){
@@ -14,25 +15,43 @@ void delay_ms(u16 ms){
 
 void main()
 {
-	_wdtc = 0xa8;//å…³é—­çœ‹é—¨ç‹—
+	_wdtc = 0xa8;//¹Ø±Õ¿´ÃÅ¹·
 	_pac3 = 0;
-	_cos = 1;//è®¾å®špa3è¾“å‡ºï¼ˆåŒæ—¶å±è”½å¤ç”¨ï¼‰
-	_pcc4 = 1;//è®¾å®šæŒ‰é”®è¾“å…¥,ä¸Šæ‹‰
+	_cos = 1;//Éè¶¨pa3Êä³ö£¨Í¬Ê±ÆÁ±Î¸´ÓÃ£©	
+	_pcc4 = 1;//Éè¶¨°´¼üÊäÈë,ÉÏÀ­
 	_pcpu4 = 1;
 	
 	LED = 1;
 	
+	//set Tm0
+	_tm0c0 = 0x30; // set f = 1/64
+	_tm0c1 = 0xc1;//timer mode and ccra match
+	_tm0al = 625 & 0xFF;//must write to Low 8-bit
+	_tm0ah = 625>>8;
+	_mf0e = 1; // multi-fun isr enable
+	_t0ae = 1; // tm0 isr enable
+	_emi = 1;// EMI 
+	_t0on = 1; //open tm0
+	
 	while(1){
-		if(KEY1 == 0){
+		/*if(KEY1 == 0){
 			delay_ms(50);
 			if(KEY1 == 0){
 				LED = ~LED;	
 			}
 		}		
-		
-	}
+		*/
+	};
 }
 //remember this function,it is defined in header file
 DEFINE_ISR(t0isr,0x0c){
-		
+	static u8 num = 0;
+	if(_t0af){
+		_t0af = 0;
+		num++;
+		if(num>200){
+			LED = ~LED;
+			num = 0;
+			}	
+	}
 }
