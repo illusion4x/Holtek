ad9acfd216534a8a8835e91aef9b22a1501ef7d7
my_mac2017 <mexuc@qq.com>
2019-04-14 09:40:51 +0800

    update

--- project_1/main.c
+++ project_1/main.c
@@ -1,11 +1,240 @@
 
-
-
-void key_fun(){
+void mode_fun(){
 	
 	switch(mode){
-		case 0:// normal 
-			
+		case normal_mode :
+			if(key_heat_up){
+				//heat_up
+				key_heat_up = 0;
+			}
+
+			if(key_cool_down){
+				//interval 5min
+				//cool down
+				key_cool_down = 0;
+			}
+
+			if(key_set){
+
+			}
+
+			if(key_heat_2){
+
+			}
+			break;
+
+		case hot_set_mode:
+			//key 
+			if(key_heat_up){
+				temp_set.hot++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.hot--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.hot<52){
+				temp_set.hot = 52;
+			}
+			else if(temp_set.hot>95){
+				temp_set.hot = 95;
+			}
+
+			//display and blink
+
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+
+			if(time_setting){
+				//save to eeprom
+			}
+
+			break;
+		case cool_set_mode:
+			if(key_heat_up){
+				temp_set.cool++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.cool--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.cool<4){
+				temp_set.hot = 4;
+			}
+			else if(temp_set.hot>15){
+				temp_set.hot = 15;
+			}
+
+			//display and blink
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+			//>10s
+			if(time_setting){
+				//save to eeprom
+			}
+			break;
+
+		case time_set : 
+			if(key_heat_up){
+				temp_set.cool++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.cool--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.cool<4){
+				temp_set.hot = 4;
+			}
+			else if(temp_set.hot>15){
+				temp_set.hot = 15;
+			}
+
+			//display and blink
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+			//>10s
+			if(time_setting){
+				//save to eeprom
+			}
+			break;
+		case time_real_set : 
+			if(key_heat_up){
+				temp_set.cool++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.cool--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.cool<4){
+				temp_set.hot = 4;
+			}
+			else if(temp_set.hot>15){
+				temp_set.hot = 15;
+			}
+
+			//display and blink
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+			//>10s
+			if(time_setting){
+				//save to eeprom
+			}
+			break;
+		case time_on_set : 
+			if(key_heat_up){
+				temp_set.cool++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.cool--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.cool<4){
+				temp_set.hot = 4;
+			}
+			else if(temp_set.hot>15){
+				temp_set.hot = 15;
+			}
+
+			//display and blink
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+			//>10s
+			if(time_setting){
+				//save to eeprom
+			}
+			break;
+		case time_set : 
+			if(key_heat_up){
+				temp_set.cool++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.cool--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.cool<4){
+				temp_set.hot = 4;
+			}
+			else if(temp_set.hot>15){
+				temp_set.hot = 15;
+			}
+
+			//display and blink
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+			//>10s
+			if(time_setting){
+				//save to eeprom
+			}
+			break;
+		case time_set : 
+			if(key_heat_up){
+				temp_set.cool++;
+				key_heat_up = 0;
+			}
+			else if(key_cool_down){
+				temp_set.cool--;
+				key_cool_down = 0;
+			}
+
+			if(temp_set.cool<4){
+				temp_set.hot = 4;
+			}
+			else if(temp_set.hot>15){
+				temp_set.hot = 15;
+			}
+
+			//display and blink
+
+			if(key_ok){
+				mode ++;
+				//save to eeprom
+				key_ok = 0
+			}
+			//>10s
+			if(time_setting){
+				//save to eeprom
+			}
+			break;
+
+		case sys_off:
+		 
+
+
 	}
 }
 
--- project_1/main.h
+++ project_1/main.h
@@ -1,4 +1,13 @@
 
+#define normal_mode
+#define hot_set_mode
+#define cool_set_mod=-0-76
+#define time_real_set
+#define time_on_setting
+
+typedef unsigned char u8;
+typedef unsigned int u16;
+
 typedef struct{
 	volatile u8 b0:1;
 	volatile u8 b1:1;
@@ -14,3 +23,37 @@ typedef union{
 	bits B;
 	u8 data;
 }key_group;
+
+key_group key_group1;
+
+#define key_group1.b1 key_heat_up
+#define key_group1.b2 key_cool_down
+#define key_group1.b3 key_set
+#define key_group1.b4 key_ok
+#define key_group1.b5 key_heat_2
+
+typedef struct{
+	u8 hot;
+	u8 cool;
+}temp_struct;
+
+time_struct temp_real;//2B
+time_struct temp_set;//2B
+
+typedef struct {	
+	u8 hour;
+	u8 min;
+}time_struct;
+
+time_struct time_real;//2B
+time_struct time_on;//2B
+time_struct time_off;//2B
+
+u8 time_setting;kk
+
+
+
+
+
+
+
